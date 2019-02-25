b6570c461622f4de2ba66aa68d461fdf974e54a0
David Cumps <david@cumps.be>
2019-02-25 19:36:46 +0100

    refactor: use generic extract builder

--- src/StreetNameRegistry.Api.Extract/Extracts/StreetNameRegistryExtractBuilder.cs
+++ src/StreetNameRegistry.Api.Extract/Extracts/StreetNameRegistryExtractBuilder.cs
@@ -1,10 +1,6 @@
 namespace StreetNameRegistry.Api.Extract.Extracts
 {
-    using Be.Vlaanderen.Basisregisters.Shaperon;
     using Projections.Extract;
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
     using System.Linq;
     using Be.Vlaanderen.Basisregisters.Api.Extract;
     using Be.Vlaanderen.Basisregisters.GrAr.Extracts;
@@ -19,49 +15,11 @@ namespace StreetNameRegistry.Api.Extract.Extracts
                 .AsNoTracking()
                 .Where(x => x.Complete);
 
-            return CreateDbfFile<StreetNameDbaseRecord>(
+            return ExtractBuilder.CreateDbfFile<StreetNameDbaseRecord>(
                 ExtractController.ZipName,
                 new StreetNameDbaseSchema(),
-                extractItems.Select(org => org.DbaseRecord),
+                extractItems.OrderBy(x => x.StreetNameOsloId).Select(org => org.DbaseRecord),
                 extractItems.Count);
         }
-
-        private static ExtractFile CreateDbfFile<TDbaseRecord>(
-            string fileName,
-            DbaseSchema schema,
-            IEnumerable<byte[]> records,
-            Func<int> getRecordCount) where TDbaseRecord : DbaseRecord, new()
-            => new ExtractFile(
-                new DbfFileName(fileName),
-                (stream, token) =>
-                {
-                    var dbfFile = CreateDbfFileWriter<TDbaseRecord>(
-                        schema,
-                        new DbaseRecordCount(getRecordCount()),
-                        stream);
-
-                    foreach (var record in records)
-                    {
-                        if (token.IsCancellationRequested)
-                            return;
-
-                        dbfFile.WriteBytesAs<TDbaseRecord>(record);
-                    }
-
-                    dbfFile.WriteEndOfFile();
-                });
-
-        private static DbfFileWriter<TDbaseRecord> CreateDbfFileWriter<TDbaseRecord>(
-            DbaseSchema schema,
-            DbaseRecordCount recordCount,
-            Stream writeStream) where TDbaseRecord : DbaseRecord
-            => new DbfFileWriter<TDbaseRecord>(
-                new DbaseFileHeader(
-                    DateTime.Now,
-                    DbaseCodePage.Western_European_ANSI,
-                    recordCount,
-                    schema
-                ),
-                writeStream);
     }
 }
