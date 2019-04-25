6d9ad96115681e33e8d60feb4448481b41cfe89e
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-04-25 10:36:32 +0200

    feat: upgrade packages

--- src/StreetNameRegistry.Api.Legacy/Infrastructure/Program.cs
+++ src/StreetNameRegistry.Api.Legacy/Infrastructure/Program.cs
@@ -1,13 +1,11 @@
 namespace StreetNameRegistry.Api.Legacy.Infrastructure
 {
-    using System;
-    using System.Security.Cryptography.X509Certificates;
     using Be.Vlaanderen.Basisregisters.Api;
     using Microsoft.AspNetCore.Hosting;
 
     public class Program
     {
-        private static readonly Tuple<string, string> DevelopmentCertificate = new Tuple<string, string>(
+        private static readonly DevelopmentCertificate DevelopmentCertificate = new DevelopmentCertificate(
             "api.dev.straatnaam.basisregisters.vlaanderen.be.pfx",
             "gemeenteregister!");
 
@@ -16,9 +14,18 @@ namespace StreetNameRegistry.Api.Legacy.Infrastructure
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
             => new WebHostBuilder()
                 .UseDefaultForApi<Startup>(
-                    httpPort: 2092,
-                    httpsPort: 2446,
-                    httpsCertificate: () => new X509Certificate2(DevelopmentCertificate.Item1, DevelopmentCertificate.Item2),
-                    commandLineArgs: args);
+                    new ProgramOptions
+                    {
+                        Hosting =
+                        {
+                            HttpPort = 2092,
+                            HttpsPort = 2446,
+                            HttpsCertificate = DevelopmentCertificate.ToCertificate,
+                        },
+                        Runtime =
+                        {
+                            CommandLineArgs = args
+                        }
+                    });
     }
 }
