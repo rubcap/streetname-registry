a861da20cac655ad0d92bb1de29c1749ca0cca20
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-04-25 14:04:31 +0200

    feat: add projector + cleanup projection libraries

---
+++ src/StreetNameRegistry.Api.Projector/Infrastructure/Modules/ApiModule.cs
@@ -0,0 +1,121 @@
+namespace StreetNameRegistry.Api.Projector.Infrastructure.Modules
+{
+    using Autofac;
+    using Autofac.Extensions.DependencyInjection;
+    using Be.Vlaanderen.Basisregisters.DataDog.Tracing.Autofac;
+    using Be.Vlaanderen.Basisregisters.EventHandling;
+    using Be.Vlaanderen.Basisregisters.EventHandling.Autofac;
+    using Be.Vlaanderen.Basisregisters.ProjectionHandling.LastChangedList;
+    using Be.Vlaanderen.Basisregisters.ProjectionHandling.SqlStreamStore.Autofac;
+    using Be.Vlaanderen.Basisregisters.Projector;
+    using Be.Vlaanderen.Basisregisters.Projector.Modules;
+    using Be.Vlaanderen.Basisregisters.Shaperon;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using StreetNameRegistry.Infrastructure;
+    using StreetNameRegistry.Projections.Extract;
+    using StreetNameRegistry.Projections.Extract.StreetNameExtract;
+    using StreetNameRegistry.Projections.LastChangedList;
+    using StreetNameRegistry.Projections.Legacy;
+    using StreetNameRegistry.Projections.Legacy.StreetNameDetail;
+    using StreetNameRegistry.Projections.Legacy.StreetNameList;
+    using StreetNameRegistry.Projections.Legacy.StreetNameName;
+    using StreetNameRegistry.Projections.Legacy.StreetNameSyndication;
+    using StreetNameRegistry.Projections.Legacy.StreetNameVersion;
+
+    public class ApiModule : Module
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IServiceCollection _services;
+        private readonly ILoggerFactory _loggerFactory;
+
+        public ApiModule(
+            IConfiguration configuration,
+            IServiceCollection services,
+            ILoggerFactory loggerFactory)
+        {
+            _configuration = configuration;
+            _services = services;
+            _loggerFactory = loggerFactory;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new DataDogModule(_configuration));
+            RegisterProjectionSetup(builder);
+
+            builder.Populate(_services);
+        }
+
+        private void RegisterProjectionSetup(ContainerBuilder builder)
+        {
+            builder.RegisterModule(
+                new EventHandlingModule(
+                    typeof(DomainAssemblyMarker).Assembly,
+                    EventsJsonSerializerSettingsProvider.CreateSerializerSettings()
+                )
+            );
+
+            builder.RegisterModule<EnvelopeModule>();
+
+            builder.RegisterEventstreamModule(_configuration);
+
+            builder.RegisterModule<ProjectorModule>();
+            RegisterExtractProjections(builder);
+            RegisterLastChangedProjections(builder);
+            RegisterLegacyProjections(builder);
+        }
+
+        private void RegisterExtractProjections(ContainerBuilder builder)
+        {
+            builder.RegisterModule(
+                new ExtractModule(
+                    _configuration,
+                    _services,
+                    _loggerFactory));
+
+            builder
+                .RegisterProjectionMigrator<ExtractContextMigrationFactory>(
+                    _configuration,
+                    _loggerFactory)
+                .RegisterProjections<StreetNameExtractProjections, ExtractContext>(
+                    () => new StreetNameExtractProjections(DbaseCodePage.Western_European_ANSI.ToEncoding()));
+        }
+
+        private void RegisterLastChangedProjections(ContainerBuilder builder)
+        {
+            builder.RegisterModule(
+                new LastChangedListModule(
+                    _configuration.GetConnectionString("LastChangedList"),
+                    _configuration["DataDog:ServiceName"],
+                    _services,
+                    _loggerFactory));
+
+            builder
+                .RegisterProjectionMigrator<StreetNameRegistry.Projections.LastChangedList.LastChangedListContextMigrationFactory>(
+                    _configuration,
+                    _loggerFactory)
+                .RegisterProjections<LastChangedProjections, LastChangedListContext>();
+        }
+
+        private void RegisterLegacyProjections(ContainerBuilder builder)
+        {
+            builder
+                .RegisterModule(
+                    new LegacyModule(
+                        _configuration,
+                        _services,
+                        _loggerFactory));
+            builder
+                .RegisterProjectionMigrator<LegacyContextMigrationFactory>(
+                    _configuration,
+                    _loggerFactory)
+                .RegisterProjections<StreetNameDetailProjections, LegacyContext>()
+                .RegisterProjections<StreetNameListProjections, LegacyContext>()
+                .RegisterProjections<StreetNameNameProjections, LegacyContext>()
+                .RegisterProjections<StreetNameSyndicationProjections, LegacyContext>()
+                .RegisterProjections<StreetNameVersionProjections, LegacyContext>();
+        }
+    }
+}
