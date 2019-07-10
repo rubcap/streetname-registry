8ca953beb10d625ff9e2ce46d7ba4ba3db7c2281
Arne Dumarey <arne.dumarey@kb.vlaanderen.be>
2019-07-10 21:31:52 +0200

    fix: fix migrations extract

---
+++ src/StreetNameRegistry.Projections.Extract/Migrations/20190710192926_RenameOsloId_PersistentLocalId.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StreetNameRegistry.Projections.Extract.Migrations
+{
+    public partial class RenameOsloId_PersistentLocalId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StreetNameOsloId",
+                schema: "StreetNameRegistryExtract",
+                table: "StreetName",
+                newName: "StreetNamePersistentLocalId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_StreetName_StreetNameOsloId",
+                schema: "StreetNameRegistryExtract",
+                table: "StreetName",
+                newName: "IX_StreetName_StreetNamePersistentLocalId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "StreetNamePersistentLocalId",
+                schema: "StreetNameRegistryExtract",
+                table: "StreetName",
+                newName: "StreetNameOsloId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_StreetName_StreetNamePersistentLocalId",
+                schema: "StreetNameRegistryExtract",
+                table: "StreetName",
+                newName: "IX_StreetName_StreetNameOsloId");
+        }
+    }
+}
