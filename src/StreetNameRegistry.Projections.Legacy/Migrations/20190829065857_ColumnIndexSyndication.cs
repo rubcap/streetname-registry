8907d63020b3318ec53c942fe327aa08f41cf71c
David Cumps <david@cumps.be>
2019-08-29 08:59:37 +0200

    fix: use columnstore for legacy syndication

---
+++ src/StreetNameRegistry.Projections.Legacy/Migrations/20190829065857_ColumnIndexSyndication.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StreetNameRegistry.Projections.Legacy.Migrations
+{
+    public partial class ColumnIndexSyndication : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "CI_StreetNameSyndication_Position",
+                schema: "StreetNameRegistryLegacy",
+                table: "StreetNameSyndication",
+                column: "Position")
+                .Annotation("SqlServer:ColumnStoreIndex", "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "CI_StreetNameSyndication_Position",
+                schema: "StreetNameRegistryLegacy",
+                table: "StreetNameSyndication");
+        }
+    }
+}
--- src/StreetNameRegistry.Projections.Legacy/StreetNameSyndication/StreetNameSyndication.cs
+++ src/StreetNameRegistry.Projections.Legacy/StreetNameSyndication/StreetNameSyndication.cs
@@ -2,6 +2,7 @@ namespace StreetNameRegistry.Projections.Legacy.StreetNameSyndication
 {
     using System;
     using Be.Vlaanderen.Basisregisters.GrAr.Provenance;
+    using Be.Vlaanderen.Basisregisters.ProjectionHandling.Runner.MigrationExtensions;
     using Infrastructure;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -108,6 +109,7 @@ namespace StreetNameRegistry.Projections.Legacy.StreetNameSyndication
                 .ForSqlServerIsClustered();
 
             b.Property(x => x.Position).ValueGeneratedNever();
+            b.HasIndex(x => x.Position).IsColumnStore($"CI_{TableName}_Position");
 
             b.Property(x => x.StreetNameId).IsRequired();
             b.Property(x => x.NisCode);
