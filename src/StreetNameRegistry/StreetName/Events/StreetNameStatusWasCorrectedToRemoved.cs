0d037506db9bf1cf15ae2c4adf6daae6d5bf6b82
MathieuBeirlaen <mathieu.beirlaen@vlaanderen.be>
2020-11-19 10:28:07 +0100

    docs: added EventPropertyDescriptions GRAR-1616

--- src/StreetNameRegistry/StreetName/Events/StreetNameStatusWasCorrectedToRemoved.cs
+++ src/StreetNameRegistry/StreetName/Events/StreetNameStatusWasCorrectedToRemoved.cs
@@ -9,7 +9,10 @@ namespace StreetNameRegistry.StreetName.Events
     [EventDescription("De straatnaam status werd gewist via correctie.")]
     public class StreetNameStatusWasCorrectedToRemoved : IHasStreetNameId, IHasProvenance, ISetProvenance
     {
+        [EventPropertyDescription("Interne GUID van de straatnaam.")]
         public Guid StreetNameId { get; }
+        
+        [EventPropertyDescription("Metadata bij het event.")]
         public ProvenanceData Provenance { get; private set; }
 
         public StreetNameStatusWasCorrectedToRemoved(StreetNameId streetNameId)
