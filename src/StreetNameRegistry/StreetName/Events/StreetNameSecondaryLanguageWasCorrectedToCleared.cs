bbe117150d6165f310a97561d6c1f341c9562383
MathieuBeirlaen <mathieu.beirlaen@vlaanderen.be>
2020-11-19 13:50:34 +0100

    docs: changed EventDescription GRAR-1616

--- src/StreetNameRegistry/StreetName/Events/StreetNameSecondaryLanguageWasCorrectedToCleared.cs
+++ src/StreetNameRegistry/StreetName/Events/StreetNameSecondaryLanguageWasCorrectedToCleared.cs
@@ -6,7 +6,7 @@ namespace StreetNameRegistry.StreetName.Events
     using Be.Vlaanderen.Basisregisters.GrAr.Provenance;
 
     [EventName("StreetNameSecondaryLanguageWasCorrectedToCleared")]
-    [EventDescription("De straatnaam secundaire taal werd gewist via correctie.")]
+    [EventDescription("De secundaire taalcode waarin de straatnaam beschikbaar is, werd gewist (via correctie).")]
     public class StreetNameSecondaryLanguageWasCorrectedToCleared : IHasStreetNameId, IHasProvenance, ISetProvenance
     {
         [EventPropertyDescription("Interne GUID van de straatnaam.")]
