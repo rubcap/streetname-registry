8b10e70cca9ac178fc36b940ef582215840022a5
MathieuBeirlaen <mathieu.beirlaen@vlaanderen.be>
2020-11-19 10:33:17 +0100

    docs: added EventPropertyDescriptions GRAR-1616

--- src/StreetNameRegistry/StreetName/Events/StreetNameWasCorrectedToProposed.cs
+++ src/StreetNameRegistry/StreetName/Events/StreetNameWasCorrectedToProposed.cs
@@ -9,7 +9,10 @@ namespace StreetNameRegistry.StreetName.Events
     [EventDescription("De straatnaam werd voorgesteld via correctie.")]
     public class StreetNameWasCorrectedToProposed : IHasStreetNameId, IHasProvenance, ISetProvenance
     {
+        [EventPropertyDescription("Interne GUID van de straatnaam.")]
         public Guid StreetNameId { get; }
+        
+        [EventPropertyDescription("Metadata bij het event.")]
         public ProvenanceData Provenance { get; private set; }
 
         public StreetNameWasCorrectedToProposed(StreetNameId streetNameId)
