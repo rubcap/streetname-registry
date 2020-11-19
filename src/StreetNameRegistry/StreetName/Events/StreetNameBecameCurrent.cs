374e5a4cf312cbc31813c6402371cd51cf60079d
MathieuBeirlaen <mathieu.beirlaen@vlaanderen.be>
2020-11-19 08:04:50 +0100

    docs: added EventPropertyDescriptions GRAR-1616

--- src/StreetNameRegistry/StreetName/Events/StreetNameBecameCurrent.cs
+++ src/StreetNameRegistry/StreetName/Events/StreetNameBecameCurrent.cs
@@ -9,7 +9,10 @@ namespace StreetNameRegistry.StreetName.Events
     [EventDescription("De straatnaam werd in gebruik genomen.")]
     public class StreetNameBecameCurrent : IHasStreetNameId, IHasProvenance, ISetProvenance
     {
+        [EventPropertyDescription("Interne GUID van de straatnaam.")]
         public Guid StreetNameId { get; }
+        
+        [EventPropertyDescription("Metadata bij het event.")]
         public ProvenanceData Provenance { get; private set; }
 
         public StreetNameBecameCurrent(StreetNameId streetNameId)
