298f52e54d4d6cd7f180df000cb6241149801d59
MathieuBeirlaen <mathieu.beirlaen@vlaanderen.be>
2020-11-19 10:40:49 +0100

    docs: added EventPropertyDescriptions GRAR-1616

--- src/StreetNameRegistry/StreetName/Events/StreetNameWasRetired.cs
+++ src/StreetNameRegistry/StreetName/Events/StreetNameWasRetired.cs
@@ -9,7 +9,10 @@ namespace StreetNameRegistry.StreetName.Events
     [EventDescription("De straatnaam werd gehistoreerd.")]
     public class StreetNameWasRetired : IHasStreetNameId, IHasProvenance, ISetProvenance
     {
+        [EventPropertyDescription("Interne GUID van de straatnaam.")]
         public Guid StreetNameId { get; }
+        
+        [EventPropertyDescription("Metadata bij het event.")]
         public ProvenanceData Provenance { get; private set; }
 
         public StreetNameWasRetired(StreetNameId streetNameId)
